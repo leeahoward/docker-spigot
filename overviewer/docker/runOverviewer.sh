a863cede83541bc4be87b0dc894ea793fbae2993
Lee Howard <leeahoward@gmail.com>
2016-08-27 19:14:46 -0400

    updated config

--- overviewer/docker/runOverviewer.sh
+++ overviewer/docker/runOverviewer.sh
@@ -2,9 +2,7 @@
 
 set -e
 set -x
-while myserver; do
+while overviewer.py -v --config=/overviewer/config/overviewer.py; do
     echo "Server 'myserver' ended with exit code $?.  Respawning.." >&2
-    overviewer.py -v --config=/overviewer/overviewer.py
-	sed -i 's/\?sensor=false\"/\&key=$MAPS_API_KEY\"/g' 
     sleep 10
 done
